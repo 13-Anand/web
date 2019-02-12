03eb78f06dacce8daf5720b2446c912653f719d3
13-Anand <anandkmr.1993@gmail.com>
2019-02-12 12:07:49 +0530

    Anand

---
+++ Angular/Demo/step4-NESTED COMPONENTS adding stars/product-viewer/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { ToUpperCasePipe } from './pipes/to-upper-case.pipe';
+import { StarsComponent } from './stars/stars.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductsComponent,
+    ToUpperCasePipe,
+    StarsComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
