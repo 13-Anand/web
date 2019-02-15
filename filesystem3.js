4ddc1e5b076f85a9b4f935f2da077a2ffffb99c5
13-Anand <anandkmr.1993@gmail.com>
2019-02-15 13:23:11 +0530

    filesystems

---
+++ filesystem3.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+console.log(__dirname);
+fs.writeFile(
+    path.join(__dirname,'/test2','Hello2.txt'),
+    'Hello!!!',
+    err => {
+        if (err) throw err;
+        console.log("File Write complete");
+    }
+);
+fs.appendFile(
+    path.join(__dirname,'/test2','Hello2.txt'),
+    'Hello123',
+    err => {
+        if (err) throw err;
+        console.log("File Write Complete!!");
+    }
+);
