b09d9f61654f858ccc9829511b3b8387d3bb82e5
13-Anand <anandkmr.1993@gmail.com>
2019-02-19 10:49:46 +0530

    web sync

--- settimeout2.js
+++ settimeout2.js
@@ -1,5 +1,5 @@
 console.log("1");
-setTimeout(()=>{console.log("2")},1000);
+setTimeout(()=>{console.log("2")},1);
 console.log("3");
-setTimeout(()=>{console.log("4")},1000);
+setTimeout(()=>{console.log("4")},1);
 console.log("5")
