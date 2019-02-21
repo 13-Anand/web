d9ddae38c04bed96fe4d83de411794999501524d
13-Anand <anandkmr.1993@gmail.com>
2019-02-21 12:26:29 +0530

    promises sync

--- promise.js
+++ promise.js
@@ -3,18 +3,27 @@ function test()
     return new Promise(function (resolve,reject)
     {
         var a = 0;
-        if(a == '1')
+        if(a === 1)
         {
             resolve();
         }
         else
         {
-            console.log("Rejected!");
+            reject();
         }
     })
 }
 test()
 .then(function () {
     console.log("Success");
-} )
-.catch()
+}, function() {
+    console.log("Failure");
+})
+.then(function (){
+    console.log("Success2");
+}, function() {
+    console.log("Failure2");
+})
+.catch(function(){
+    console.log("Rejected!");
+})
